e17e40d922c7d7aee26deec03b3a1354016bce95
khurram <youthpassion.com@gmail.com>
2026-03-15 22:40:43 +0500

    minor

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const server = http.createServer((req, res) => {
 });
 const port = process.env.PORT || 3000;
 server.listen(port, '0.0.0.0', () => {
-  console.log('Server listening on port ' + port);
+  console.log('Server listening on port. ' + port);
 });
